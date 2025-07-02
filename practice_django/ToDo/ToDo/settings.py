f2b08e654738e53178e8122c5c06a7b877c98ace
sam-1388 <sam200x.sy@gmail.com>
2025-07-02 18:31:47 +0300

    s

--- practice_django/ToDo/ToDo/settings.py
+++ practice_django/ToDo/ToDo/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Task',
-    'rest_framework',
-    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
 ]
 
 REST_FRAMEWORK={
